b5f499f042b79242af081cec07d457da86aa954c
toolsa076-ai <toolsa076@gmail.com>
2026-09-26 00:47:19 -0700

    Create day_3.py

---
+++ day_3.py
@@ -0,0 +1,12 @@
+# type conversion & type casting
+num = input("enter your number ")
+print(int(num) + 8)
+print(1 + 5) # impicit
+print (int(num) + 6)
+print(3 + (int(3.8888))) # explicit
+new_age = input("enter your number")
+print(float(new_age) + 9)
+
+print(bool(new_age) + 5)
+sum = int(num) + float(new_age)
+print(sum)S
