bcd2922354ad6afdf521bf5ccac58b74cc479f47
toolsa076-ai <toolsa076@gmail.com>
2026-09-26 08:50:15 -0700

    day4-.PY

---
+++ day_4.py
@@ -0,0 +1,45 @@
+name = "fatima"
+age = 21
+city = "lahore"
+print(name + str(age) + city)
+my_favourite_colour = "pink"
+print(my_favourite_colour)
+my_favourite_food = "broast"
+print(my_favourite_food)
+a = 10
+b= 20
+c = a + b
+print(c)
+x = "10"
+x = int(x)
+print(x + 10)
+naam = input("enter your name:")
+print("hello" + naam)
+a = 18
+b = 87
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+x = 10 
+y = 20 
+print(x > y)
+print(x % y)
+numbers = 70 
+if numbers > 60:
+    print("A+")
+else:
+    print("A")    
+x = 6
+print(6 % 2 == 0)    
+y = 8
+print(8 % 3 ==3)
+for i in range(1, 9):
+    print(i)
+    for i in range(1 , 34):
+        if i % 2 == 0:
+            print(i)
+for i in range (2 , 88):
+    if i % 3 ==1:
+        print(i)            
